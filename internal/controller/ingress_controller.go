ec1a2c6d1106fd86bdaace380da768ac9703cb35
Gerard de Leeuw <gdeleeuw@leeuwit.nl>
2024-12-02 14:21:04 +0100

    Add logic

--- internal/controller/ingress_controller.go
+++ internal/controller/ingress_controller.go
@@ -18,6 +18,14 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"strings"
+
+	"github.com/lion7/ingress2httproute/internal/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -50,9 +58,147 @@ type IngressReconciler struct {
 func (r *IngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
-	logger.Info("Reconciling Ingress")
+	ingress := networkingv1.Ingress{}
+	if err := r.Get(ctx, req.NamespacedName, &ingress); err != nil {
+		logger.Error(err, fmt.Sprintf("cannot reconcile ingress %s", req.NamespacedName))
+		if errors.IsNotFound(err) {
+			if err := utils.DeleteIfExists[gatewayv1.HTTPRoute](r.Client, ctx, req.NamespacedName); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
 
+	// map the hostnames to Gateway API format
+	var hostnames []gatewayv1.Hostname
+	for _, rule := range ingress.Spec.Rules {
+		if rule.Host != "" {
+			if len(hostnames) == 0 {
+				hostnames = append(hostnames, gatewayv1.Hostname(rule.Host))
+			} else if expected := string(hostnames[0]); rule.Host != expected {
+				return ctrl.Result{}, fmt.Errorf("hostname mismatch, expected %s but got %s", expected, rule.Host)
+			}
+		}
+	}
+	if hostnames == nil {
+		logger.Info("no hostnames specified")
+		return ctrl.Result{}, nil
+	}
+
+	// map the rules to Gateway API format
+	var rules []gatewayv1.HTTPRouteRule
+	for _, rule := range ingress.Spec.Rules {
+		if rule.HTTP != nil {
+			var matches []gatewayv1.HTTPRouteMatch
+			var backendRefs []gatewayv1.HTTPBackendRef
+			for _, path := range rule.HTTP.Paths {
+				pathMatch := gatewayv1.HTTPPathMatch{}
+				pathMatch.Value = &path.Path
+				if path.PathType == nil {
+					pathMatch.Type = nil
+				} else if *path.PathType == networkingv1.PathTypeExact {
+					exact := gatewayv1.PathMatchExact
+					pathMatch.Type = &exact
+				} else if *path.PathType == networkingv1.PathTypePrefix {
+					prefix := gatewayv1.PathMatchPathPrefix
+					pathMatch.Type = &prefix
+				}
+				matches = append(matches, gatewayv1.HTTPRouteMatch{Path: &pathMatch})
+
+				ref, err := mapBackendRef(ctx, r.Client, req.Namespace, path.Backend)
+				if err != nil {
+					return ctrl.Result{}, err
+				}
+				backendRefs = append(backendRefs, ref)
+			}
+			if len(backendRefs) > 0 && ingress.Spec.DefaultBackend != nil {
+				ref, err := mapBackendRef(ctx, r.Client, req.Namespace, *ingress.Spec.DefaultBackend)
+				if err != nil {
+					return ctrl.Result{}, err
+				}
+				backendRefs = append(backendRefs, ref)
+			}
+			rules = append(rules, gatewayv1.HTTPRouteRule{
+				Matches:     matches,
+				BackendRefs: backendRefs,
+			})
+		}
+	}
+	if rules == nil {
+		logger.Info("no rules specified")
+		return ctrl.Result{}, nil
+	}
+
+	// Find matching gateways
+	var parentRefs []gatewayv1.ParentReference
+	var gateways gatewayv1.GatewayList
+	if err := r.List(ctx, &gateways); err != nil {
+		logger.Error(err, "cannot list gateways")
+		return ctrl.Result{}, err
+	}
+	for _, gateway := range gateways.Items {
+	listeners:
+		for _, listener := range gateway.Spec.Listeners {
+			nsSelector := gatewayv1.NamespacesFromSame
+			if listener.AllowedRoutes != nil && listener.AllowedRoutes.Namespaces != nil && listener.AllowedRoutes.Namespaces.From != nil {
+				nsSelector = *listener.AllowedRoutes.Namespaces.From
+			}
+			nsMatches := nsSelector == gatewayv1.NamespacesFromAll || (nsSelector == gatewayv1.NamespacesFromSame && gateway.Namespace == req.Namespace)
+			if !nsMatches {
+				continue
+			}
+			if listener.Hostname == nil {
+				continue
+			}
+			hostname := string(*listener.Hostname)
+			if strings.HasPrefix(hostname, "*.") {
+				fqdn := strings.TrimPrefix(hostname, "*.")
+				for _, s := range hostnames {
+					if strings.HasSuffix(string(s), fqdn) {
+						parentRefs = append(parentRefs, createParentRef(gateway, listener.Name))
+						continue listeners
+					}
+				}
+			} else {
+				for _, s := range hostnames {
+					if strings.EqualFold(string(s), hostname) {
+						parentRefs = append(parentRefs, createParentRef(gateway, listener.Name))
+						continue listeners
+					}
+				}
+			}
+		}
+	}
+	if parentRefs == nil {
+		logger.Info("could not find matching gateways")
+		return ctrl.Result{}, nil
+	}
+
+	// Create the owner reference
+	bTrue := true
+	owner := metav1.OwnerReference{
+		APIVersion:         ingress.APIVersion,
+		Kind:               ingress.Kind,
+		Name:               ingress.Name,
+		UID:                ingress.UID,
+		Controller:         &bTrue,
+		BlockOwnerDeletion: &bTrue,
+	}
+	spec := gatewayv1.HTTPRouteSpec{
+		CommonRouteSpec: gatewayv1.CommonRouteSpec{ParentRefs: parentRefs},
+		Hostnames:       hostnames,
+		Rules:           rules,
+	}
+
+	_, err := utils.GetOrCreate[gatewayv1.HTTPRoute](ctx, r.Client, req.NamespacedName, owner, func() (*gatewayv1.HTTPRoute, error) {
+		return &gatewayv1.HTTPRoute{Spec: spec}, nil
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	logger.Info("created equivalent HTTPRoute for ingress")
 	return ctrl.Result{}, nil
 }
 
@@ -62,4 +208,53 @@ func (r *IngressReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&networkingv1.Ingress{}).
 		Owns(&gatewayv1.HTTPRoute{}).
 		Complete(r)
+}
+
+func createParentRef(gateway gatewayv1.Gateway, section gatewayv1.SectionName) gatewayv1.ParentReference {
+	gvk := gateway.GroupVersionKind()
+	group := gatewayv1.Group(gvk.Group)
+	kind := gatewayv1.Kind(gvk.Kind)
+	namespace := gatewayv1.Namespace(gateway.Namespace)
+	name := gatewayv1.ObjectName(gateway.Name)
+	return gatewayv1.ParentReference{
+		Group:       &group,
+		Kind:        &kind,
+		Namespace:   &namespace,
+		Name:        name,
+		SectionName: &section,
+	}
+}
+
+func mapBackendRef(ctx context.Context, c client.Client, namespace string, ref networkingv1.IngressBackend) (gatewayv1.HTTPBackendRef, error) {
+	var objectRef gatewayv1.BackendObjectReference
+	if ref.Resource != nil {
+		if ref.Resource.APIGroup != nil {
+			group := gatewayv1.Group(*ref.Resource.APIGroup)
+			objectRef.Group = &group
+		}
+		kind := gatewayv1.Kind(ref.Resource.Kind)
+		name := gatewayv1.ObjectName(ref.Resource.Name)
+		objectRef.Kind = &kind
+		objectRef.Name = name
+	} else if ref.Service != nil {
+		name := gatewayv1.ObjectName(ref.Service.Name)
+		objectRef.Name = name
+		if ref.Service.Port.Number != 0 {
+			port := gatewayv1.PortNumber(ref.Service.Port.Number)
+			objectRef.Port = &port
+		} else if ref.Service.Port.Name != "" {
+			svc := corev1.Service{}
+			if err := c.Get(ctx, types.NamespacedName{Namespace: namespace, Name: ref.Service.Name}, &svc); err != nil {
+				return gatewayv1.HTTPBackendRef{}, err
+			}
+			for _, svcPort := range svc.Spec.Ports {
+				if svcPort.Name == ref.Service.Port.Name {
+					port := gatewayv1.PortNumber(svcPort.Port)
+					objectRef.Port = &port
+					break
+				}
+			}
+		}
+	}
+	return gatewayv1.HTTPBackendRef{BackendRef: gatewayv1.BackendRef{BackendObjectReference: objectRef}}, nil
 }
---
+++ internal/utils/client_utils.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func GetOrCreate[K interface{}](ctx context.Context, c client.Client, key types.NamespacedName, owner metav1.OwnerReference, factory func() (*K, error)) (*K, error) {
+	resource := new(K)
+	obj, ok := interface{}(resource).(client.Object)
+	if !ok {
+		return nil, nil
+	}
+	if err := c.Get(ctx, key, obj); err != nil {
+		if errors.IsNotFound(err) {
+			if err := c.Create(ctx, &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: "capi-system"}}); err != nil {
+				if !errors.IsAlreadyExists(err) {
+					return nil, err
+				}
+			}
+			result, err := factory()
+			if err != nil {
+				return nil, err
+			}
+			newObj, ok := interface{}(result).(client.Object)
+			if !ok {
+				return nil, nil
+			}
+			newObj.SetNamespace(key.Namespace)
+			newObj.SetName(key.Name)
+			newObj.SetOwnerReferences([]metav1.OwnerReference{owner})
+			if err := c.Create(ctx, newObj); err != nil {
+				return nil, err
+			}
+			return result, nil
+		} else {
+			return nil, err
+		}
+	}
+	return resource, nil
+}
+
+func DeleteIfExists[K interface{}](c client.Client, ctx context.Context, key types.NamespacedName) error {
+	resource := new(K)
+	obj, ok := interface{}(resource).(client.Object)
+	if !ok {
+		return nil
+	}
+	if err := c.Get(ctx, key, obj); err != nil {
+		if errors.IsNotFound(err) {
+			return nil
+		} else {
+			return err
+		}
+	}
+	if err := c.Delete(ctx, obj); err != nil {
+		return err
+	}
+	return nil
+}
